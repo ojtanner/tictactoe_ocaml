c83954aa9e44eb472b8c968c6925ed48ae635ce5
Oliver Julian Taner <oliver-tanner@outlook.com>
2020-05-28 01:35:50 +0200

    Improve CLI Printing

--- lib/cli.ml
+++ lib/cli.ml
@@ -1,31 +1,37 @@
+open Base
+open Core
+
 let row_to_string (row : int list) =
   let delimiter = "|" in
-  let row = List.fold_left
-    (fun accumulator number ->
-       let mark =
-         if number = -1 then " x "
-         else if number = 1 then " O "
-         else "   " in
-       let mark_with_delimiter = mark ^ delimiter
-       in
-       accumulator ^ mark_with_delimiter
-    )
-    delimiter
-    row
+  let row =
+    List.fold_left row ~init:delimiter ~f:(fun accumulator number ->
+        let mark = if number = -1 then " x " else if number = 1 then " O " else "   " in
+        let mark_with_delimiter = mark ^ delimiter in
+        accumulator ^ mark_with_delimiter)
   in
   row ^ "\n"
+;;
 
-let generate_row_separator row =
-  let row_length = String.length row in
-  let row_delimiter = String.make row_length '-' in
-  row_delimiter ^ "\n"
+let generate_base_separator (row : int list) =
+  let piece = '-' in
+  let intersection = "+" in
+  String.concat ~sep:intersection (List.map row ~f:(fun _ -> String.make 3 piece))
+;;
+
+let generate_row_separator (row : int list) =
+  let delimiter = "|" in
+  delimiter ^ generate_base_separator row ^ delimiter ^ "\n"
+;;
+
+let generate_top_bottom_separator (row : int list) =
+  let delimiter = "+" in
+  delimiter ^ generate_base_separator row ^ delimiter ^ "\n"
+;;
 
 let print_playing_field playing_field =
-  List.iter
-    (fun row ->
-       let row_string = row_to_string row in
-       let row_delimiter = generate_row_separator row_string in
-       print_string row_string;
-       print_string row_delimiter;
-    )
-    playing_field
+  let separator = generate_row_separator [ 0; 0; 0 ] in
+  let top_bottom_separator = generate_top_bottom_separator [ 0; 0; 0 ] in
+  let rows = List.map playing_field ~f:(fun row -> row_to_string row) in
+  let rows = String.concat ~sep:separator rows in
+  print_string (top_bottom_separator ^ rows ^ top_bottom_separator)
+;;
