97ec63908428f1a563d65d9ab57d7a721e5c1034
Oliver Julian Taner <oliver-tanner@outlook.com>
2020-06-04 20:31:23 +0200

    Add Documentation Comments To Game Mli

--- lib/game.mli
+++ lib/game.mli
@@ -3,6 +3,8 @@ type t
 (** [create ()] creates a new game-state. *)
 val create : unit -> t
 
+(** [to_list t] takes the state t of a game and converts that state to a list of strings that can be printed. *)
 val to_list : t -> string list
 
+(** [execute_turn t coord] takes the state t of a game and the fieldcoordinates coord and then executes current turn based off of the current player and the coord of the playingfield he specified. *)
 val execute_turn : t -> Fieldcoordinates.t -> t
