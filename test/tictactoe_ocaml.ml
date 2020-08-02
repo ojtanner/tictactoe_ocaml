d48f9788b39327f6d118592de9ac52c036a4db24
Oliver Julian Taner <oliver-tanner@outlook.com>
2020-08-02 03:17:44 +0200

    Fix Broken Changes

--- test/tictactoe_ocaml.ml
+++ test/tictactoe_ocaml.ml
@@ -17,10 +17,9 @@ let game_tests =
          let open Player in
          let expected_player = O in
          let game = create () in
-         let t1 = from_int 1 1 in
          let actual_player =
-           match t1 with
-           | Ok t1 -> execute_turn game t1 |> get_current_player
+           match from_int 1 1 >>= fun coord -> execute_turn game coord with
+           | Ok game -> get_current_player game
            | _ -> None
          in
          assert_equal expected_player actual_player)
@@ -32,30 +31,39 @@ let game_tests =
          let open Player in
          let game = create () in
          let expected_winner = X in
-         let t1 = from_int 1 1
-         and t2 = from_int 2 1
-         and t3 = from_int 1 2
-         and t4 = from_int 2 2
-         and t5 = from_int 1 3
-         and t6 = from_int 2 2 in
          let actual_winner =
-           match t1, t2, t3, t4, t5, t6 with
-           | Ok t1, Ok t2, Ok t3, Ok t4, Ok t5, Ok t6 ->
-             print_game game;
-             let turn1 = execute_turn game t1 in
-             print_game turn1;
-             let turn2 = execute_turn turn1 t2 in
-             print_game turn2;
-             let turn3 = execute_turn turn2 t3 in
-             print_game turn3;
-             let turn4 = execute_turn turn3 t4 in
-             print_game turn4;
-             let turn5 = execute_turn turn4 t5 in
-             print_game turn5;
-             let turn6 = execute_turn turn5 t6 in
-             print_game turn6;
-             get_winner turn6
-           | _, _, _, _, _, _ -> None
+           from_int 1 1
+           >>= fun t1 ->
+           from_int 2 1
+           >>= fun t2 ->
+           from_int 1 2
+           >>= fun t3 ->
+           from_int 2 2
+           >>= fun t4 ->
+           from_int 1 3
+           >>= fun t5 ->
+           print_game game;
+           execute_turn game t1
+           >>= fun turn1 ->
+           print_game turn1;
+           execute_turn turn1 t2
+           >>= fun turn2 ->
+           print_game turn2;
+           execute_turn turn2 t3
+           >>= fun turn3 ->
+           print_game turn3;
+           execute_turn turn3 t4
+           >>= fun turn4 ->
+           print_game turn4;
+           execute_turn turn4 t5
+           >>| fun turn5 ->
+           print_game turn5;
+           get_winner turn5
+         in
+         let actual_winner =
+           match actual_winner with
+           | Ok winner -> winner
+           | _ -> None
          in
          assert_equal expected_winner actual_winner)
        ]
